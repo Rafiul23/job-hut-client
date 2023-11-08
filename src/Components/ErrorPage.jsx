0cad18e3c8e07c16c9ca84b0e41fe7eee9fe1766
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-11-09 02:16:54 +0600

    errorpage is changed

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,12 +1,10 @@
 import { Link } from "react-router-dom";
-
+import image from '../assets/notFound.gif'
 
 const ErrorPage = () => {
     return (
-        <div className="mx-auto mt-40 space-y-4 text-center">
-            <h2 className="text-3xl font-bold">Oppss!!!</h2>
-            <h1 className="text-4xl font-bold text-red-600">404</h1>
-            <p className="text-2xl">Something went wrong!!!</p>
+        <div className="mx-auto space-y-4 text-center">
+            <img src={image} className="mx-auto" alt="" />
             <Link to="/"><button className="btn mt-5 btn-warning">Go back to Home</button></Link>
         </div>
     );
