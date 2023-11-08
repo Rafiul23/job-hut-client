a2d649bfa07830b0db860b054a246d2a746bce18
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-11-08 17:05:25 +0600

    posting job in database is successfull

--- src/Components/AddJobs.jsx
+++ src/Components/AddJobs.jsx
@@ -1,23 +1,80 @@
+import { useContext } from "react";
+import { AuthContext } from "./AuthProvider";
+
 
 
 const AddJobs = () => {
+
+    const {user} = useContext(AuthContext);
+
+    const email = user?.email;
+    
+
+    const handleAddJob = e =>{
+        e.preventDefault();
+        const form = e.target;
+        
+        const job_banner = form.bannerPic.value;
+        const  job_title = form.jobTitle.value;
+        const employee_name = form.employeeName.value;
+        const job_category = form.jobCategory.value;
+        const salary_range = form.salaryRange.value;
+        const job_description = form.description.value;
+        const job_posting_date = form.postedDate.value;
+        const application_deadline = form.deadline.value;
+        const job_applicants_number = form.applicantNumber.value;
+
+        const newJob = {
+             email,
+             job_banner, 
+             job_title, 
+             employee_name, 
+             job_category, 
+             salary_range, 
+             job_description, 
+             job_posting_date,
+             application_deadline, 
+             job_applicants_number 
+        }
+
+        console.log(newJob);
+
+        fetch('http://localhost:5000/jobs', {
+            method: 'POST',
+            headers: {
+              "content-type": "application/json"
+            },
+            body: JSON.stringify(newJob)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.insertedId){
+                alert('New job added successfully');
+            }
+        })
+        .catch(error => {
+            console.log(error.message);
+        })
+    }
+
     return (
         <div>
              <h2 className="text-3xl text-center my-4 font-bold text-[#045efa]">Add a Job</h2>
-            <form className="card-body bg-[#9eb9f8] mb-4 rounded-lg">
+
+            <form onSubmit={handleAddJob} className="card-body bg-[#9eb9f8] mb-4 rounded-lg">
                 {/* banner and title */}
                <div className="flex md:flex-row flex-col">
                <div className="form-control w-full md:w-1/2 mr-5">
                     <label className="label">
                         <span className="label-text">Banner Picture</span>
                     </label>
-                    <input type="text" placeholder="Type url for banner picture" className="input input-bordered" required />
+                    <input type="text" name="bannerPic" placeholder="Type url for banner picture" className="input input-bordered" required />
                 </div>
                 <div className="form-control w-full md:w-1/2">
                     <label className="label">
                         <span className="label-text">Job Title</span>
                     </label>
-                    <input type="text" placeholder="Job Title" className="input input-bordered" required />
+                    <input type="text" name="jobTitle" placeholder="Job Title" className="input input-bordered" required />
                 </div>
                </div>
                {/* username and job category */}
@@ -26,13 +83,13 @@ const AddJobs = () => {
                     <label className="label">
                         <span className="label-text">Username</span>
                     </label>
-                    <input type="text" placeholder="Username" className="input input-bordered" required />
+                    <input type="text" name="employeeName" placeholder="Username" className="input input-bordered" required />
                 </div>
                 <div className="form-control w-full md:w-1/2">
                     <label className="label">
                         <span className="label-text">Job Category</span>
                     </label>
-                    <input type="text" placeholder="Job Category" className="input input-bordered" required />
+                    <input type="text" name="jobCategory" placeholder="Job Category" className="input input-bordered" required />
                 </div>
                </div>
                {/* salary range and job description */}
@@ -41,13 +98,13 @@ const AddJobs = () => {
                     <label className="label">
                         <span className="label-text">Salary Range</span>
                     </label>
-                    <input type='text' placeholder="Salary Range" className="input input-bordered" required />
+                    <input type='text' name="salaryRange" placeholder="Salary Range" className="input input-bordered" required />
                 </div>
                 <div className="form-control w-full md:w-1/2">
                     <label className="label">
                         <span className="label-text">Job Description</span>
                     </label>
-                    <input type="text" placeholder="Job Description" className="input input-bordered" required />
+                    <input type="text" name="description" placeholder="Job Description" className="input input-bordered" required />
                 </div>
                </div>
                {/* post date and deadline */}
@@ -56,13 +113,13 @@ const AddJobs = () => {
                     <label className="label">
                         <span className="label-text">Job Post Date</span>
                     </label>
-                    <input type="date" placeholder="Job Post Date" className="input input-bordered" required />
+                    <input type="date" name="postedDate" placeholder="Job Post Date" className="input input-bordered" required />
                 </div>
                 <div className="form-control w-full md:w-1/2">
                     <label className="label">
                         <span className="label-text">Application Deadline</span>
                     </label>
-                    <input type="date" placeholder="Application Deadline" className="input input-bordered" required />
+                    <input type="date" name="deadline" placeholder="Application Deadline" className="input input-bordered" required />
                 </div>
                </div>
                {/* Applicants number */}
@@ -71,7 +128,7 @@ const AddJobs = () => {
                     <label className="label">
                         <span className="label-text">Job Applicants Number</span>
                     </label>
-                    <input type="number" placeholder="Job Applicants Number" className="input input-bordered" required />
+                    <input type="number" placeholder="Job Applicants Number" name='applicantNumber' className="input input-bordered" required />
                 </div>
                
                </div>
--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
         <div className="mx-auto mt-40 space-y-4 text-center">
             <h2 className="text-3xl font-bold">Oppss!!!</h2>
             <h1 className="text-4xl font-bold text-red-600">404</h1>
-            <p className="text-2xl">Page not found or the products of this brand is not available</p>
+            <p className="text-2xl">Something went wrong!!!</p>
             <Link to="/"><button className="btn mt-5 btn-warning">Go back to Home</button></Link>
         </div>
     );
